696be17b660411285c769fc704bf69bbb52ea264
Bharathraj8668 <bharathraj222005@gmail.com>
2026-08-10 19:23:53 +0530

    filed modified

--- tests/webtable.spec.js
+++ tests/webtable.spec.js
@@ -1,5 +1,5 @@
 import{test,expect}from'@playwright/test'
-test('Webtable',({page})=>{
+test('Webtable', async({page})=>{
     await page.goto('https://testautomationpractice.blogspot.com/')
     const rowdata=await page.locator('//table[@name="BookTable"]//tbody//tr[1]//td').allTextContents()
     console.log(rowdata)
